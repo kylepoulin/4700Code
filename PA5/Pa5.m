c69a75b11c375aa0ea4e689c7ed825fe3706f9d2
Kyle Poulin <kylepoulin@AD.DOE.CARLETON.CA>
2018-02-08 17:59:30 -0500

    PA5 level up

---
+++ PA5/Pa5.m
@@ -0,0 +1,45 @@
+nx = 50;
+ny = 50;
+
+deltaxy = 1;
+
+G = sparse(nx*ny);
+B = zeros(1,nx*ny);
+
+
+for i = 1:nx
+    for j = 1:ny
+        n= j + (i-1)*ny;
+        
+        if i==1
+            G(n,:) = 0;
+            G(n,n) = 1;
+        elseif i == nx
+            G(n,:) = 0;
+            G(n,n) = 1;
+        elseif j ==1
+            G(:,n) = 0;
+            G(n,n) = 1;
+        elseif j ==ny
+            G(:,n) = 0;
+            G(n,n) = 1;
+        else
+            G(n,n) = -4;
+            G(n,j+(i-2)*ny) = 1;
+            G(n,j+i*ny) = 1;
+            G(n,j+1+(i-1)*ny) = 1;
+            G(n,j-1+(i-1)*ny ) = 1;
+        end
+    end   
+end
+
+[E,D] = eigs(G,9,'SM');
+
+V = zeros(50,50,9);
+
+for i= 1:9
+    V(:,:,i) = reshape(E(:,i), nx, ny);
+    
+end
+surf(V(:,:,1));
+
